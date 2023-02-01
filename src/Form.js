3817e83d24ca9d0da47fb52d8c5466579232999d
linusgun <deym2@hotmail.com>
2023-02-01 12:31:20 -0400

    I have updated the form, style and content. Pending: Send email action

--- src/Form.js
+++ src/Form.js
@@ -1,24 +1,66 @@
+import FormCSS from "./Form.module.css"
+
 export default function Form() {
   return (
-    <form>
-      <h4>Has tu consulta</h4>
-      <input type="text" placeholder="Primer Nombre"></input>
-      <input type="text" placeholder="Segundo Nombre"></input>
-      <input type="email" placeholder="email"></input>
-      <input type="text" placeholder="Telefono"></input>
-      <input type="text" placeholder="Compañia"></input>
-      <input type="text" placeholder="País"></input>
-      <select id="industria" name="industria">
-        <option value="volvo">Aplicacion</option>
-        <option value="saab">Materiales</option>
-        <option value="saab">Materiales</option>
-      </select>
-      <select id="aplicacion" name="aplicacion">
-        <option value="volvo">Aplicacion</option>
-        <option value="saab">Materiales</option>
-        <option value="saab">Materiales</option>
-      </select>
-      <textarea placeholder="Mensaje"></textarea>
-    </form>
+    <section className={FormCSS.container}>
+      <form className={FormCSS.contactForm}>
+        <h4 className={FormCSS.title}>Has tu consulta</h4>
+        <div className={FormCSS.subSec}>
+          <input
+            className={FormCSS.formItem}
+            type="text"
+            placeholder="Primer Nombre"
+          ></input>
+          <input
+            className={FormCSS.formItem}
+            type="text"
+            placeholder="Segundo Nombre"
+          ></input>
+          <input
+            className={FormCSS.formItem}
+            type="email"
+            placeholder="email"
+          ></input>
+        </div>
+        <div className={FormCSS.subSec}>
+          <input
+            className={FormCSS.formItem}
+            type="number"
+            placeholder="Telefono"
+          ></input>
+          <input
+            className={FormCSS.formItem}
+            type="text"
+            placeholder="Compañia"
+          ></input>
+          <input
+            className={FormCSS.formItem}
+            type="text"
+            placeholder="País"
+          ></input>
+        </div>
+        <div className={FormCSS.subSec}>
+          <select
+            className={[FormCSS.formItem, FormCSS.optIndustrias].join(" ")}
+            id="industria"
+            name="industria"
+            defaultValue=""
+          >
+            <option value=""></option>
+            <option value="joyeria">Joyeria</option>
+            <option value="fundicion">Fundicion</option>
+          </select>
+        </div>
+        <div className={FormCSS.subSec}>
+          <textarea
+            className={[FormCSS.formItem, FormCSS.textArea].join(" ")}
+            placeholder="Mensaje"
+          ></textarea>
+        </div>
+        <div className={FormCSS.subSec}>
+          <input className={[FormCSS.boton]} type="button" value="Enviar" />
+        </div>
+      </form>
+    </section>
   )
 }
